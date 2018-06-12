08965d6998b976ddc8c901ded35de244063c2db9
rozad <rozad@CRA-DROZADA>
2018-06-11 23:57:41 -0400

    06/11/18 Adding the CropView class

---
+++ cityOfAaron/src/byui/cit260/cityOfAaron/view/CropView.java
@@ -0,0 +1,15 @@
+// The CropView class - part of the view layer
+// Object of this class manages the crop view 
+// CIT - 260 
+// Author: Patricia Struk, Yamilka Cuellar, Alejandra Canales
+// Date last modified: June/11/2018
+//----------------------------------------------------------------
+package byui.cit260.cityOfAaron.view;
+
+/**
+ *
+ * @author rozad
+ */
+public class CropView {
+    
+}
