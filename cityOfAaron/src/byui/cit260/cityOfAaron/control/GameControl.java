5bf5163a6d851d94abe15a98edb191f902d06efd
aleja <aleja@LAPTOP-8E2DEDN3>
2018-06-28 19:00:15 -0600

    06/28/2018 - Authors: Yamilka Cuellar, Patricia Struk, Alejandra Canales Lesson Ten Team Assignment created in two parts including this commit which includes the Map Locations in GameControl. 06/27/2018 - Created GameControl class in the control layer, Map class in the model layer and moved theGame, thePlayer, and the CropData objects from the MainMenuView to the GameControl class. We created a list of animals and code to display it. Updated GameMenuView and PrintListMenuView.

--- cityOfAaron/src/byui/cit260/cityOfAaron/control/GameControl.java
+++ cityOfAaron/src/byui/cit260/cityOfAaron/control/GameControl.java
@@ -146,7 +146,8 @@ public static void createMap()   {
     
 // set a farmland location with a hint
     loc = new Location();
-    loc.setDescription(farmland + "\nOne person can plant ten acres of wheat.");
+    loc.setDescription(farmland + "\nOne person can tend ten acres of land"
+            + "\nand one bushel of wheat can plant two acres.");
     loc.setSymbol("!!!");
     theMap.setLocation(1,3,loc);
     theMap.setLocation(2,3,loc);
@@ -171,37 +172,144 @@ public static void createMap()   {
     for(int i = 0; i < MAX_ROW; i++){
       theMap.setLocation(i,0,loc);
       
-      
     }
     
     
+    //PLAYGROUND--------------------------------------------------------------------   
+    // define the string for a Playground location
+    String playground = "\nThis is the local playground. It is a safe place for"+
+    "\nyoung children to play and where many people come to enjoy the sunshine"+
+    "\nand a picnic." ;
+    
+    // set a playground location 
+    loc = new Location();
+    loc.setDescription(playground);
+    loc.setSymbol("[*]");
+    theMap.setLocation(0,1,loc);
+    
+    
+    //CITY OF AARON (CAPITAL CITY) --------------------------------------------------------------------   
+    // define the string for the city of Aaron location
+    String city = "\nWelcome to the capital city of Aaron. It is a humble town"+
+    "\nof warm and friendly people. On Saturdays, merchants come from all around"+
+    "\nto trade their goods and services at the market." ;
+    
+    // set a city of Aaron location 
+    loc = new Location();
+    loc.setDescription(city);
+    loc.setSymbol("$$$");
+    theMap.setLocation(1,1,loc);
+    
+    //TEMPLE--------------------------------------------------------------------   
+    // define the string for the Temple location
+    String temple = "\nWelcome to the House of the Lord. This is a holy place "+
+    "\nwhere people can find peace and comfort. While you are here, make sure to"+
+    "\nattend the visitors center." ;
+    
+    // set a temple location 
+    loc = new Location();
+    loc.setDescription(temple);
+    loc.setSymbol("|=|");
+    theMap.setLocation(2,1,loc);
+    
+    
+    //CHIEF JUDGE/COURTHOUSE---------------------------------------------------- 
+    // define the string for the courthouse location
+    String courthouse = "\nWe are a law abiding people. Our chief judge, Nephi"+
+    "\nrules with justice, compassion, and wisdom. We adhere to the law of "+
+    "\nconsecration." ;
+    
+    // set a courthouse location 
+    loc = new Location();
+    loc.setDescription(courthouse);
+    loc.setSymbol("###");
+    theMap.setLocation(3,1,loc);
     
     
     
+    //FOREST-------------------------------------------------------------------
+    // define the string for the forest location
+    String forest = "\nYou are standing in the Forest of No Return. Some of"+
+    "\nour most dangerous creatures live among these trees. Don't get too close"+
+    "\nor you might regret it. You might even see some Gadianton Robbers" ;
+    
+    // set a forest location 
+    loc = new Location();
+    loc.setDescription(forest);
+    loc.setSymbol("YYY");
+    theMap.setLocation(4,1,loc);
+    
+    
+    //TOOLSHED------------------------------------------------------------------
+    // define the string for the toolshed location
+    String toolshed = "\nDoing a renovation? Need some tools? You have come to"+
+    "\nthe right place. The tool shed holds the largest assortment of tools in"+
+    "\nthe region." ;
+    
+    // set a toolshed location and leave a hint
+    loc = new Location();
+    loc.setDescription(toolshed + "\nTo see a list of the tools available, go"
+            + "\nto the game menu and select option 2");
+    loc.setSymbol("TTT");
+    theMap.setLocation(0,2,loc);
+    
+    
+    //PASTURE AND ANIMALS-------------------------------------------------------
+    // define the string for the pasture location
+    String pasture = "\nYou are standing in the middle of verdant pastures"+
+    "\nwhere a wide variety of animals and livestock find respite. Come check"+
+    "\nout our cureloms and cummoms. We bet you've never seen those before!" ;
+    
+    // set a pasture location and leave a hint
+    loc = new Location();
+    loc.setDescription(pasture + "\nTo see a list of our animals, go"
+            + "\nto the game menu and select option 2");
+    loc.setSymbol("xxx");
+    theMap.setLocation(3,3,loc);
+    theMap.setLocation(4,3,loc);
+
+    
+    //STOREHOUSE----------------------------------------------------------------
+    // define the string for the storehouse location
+    String storehouse = "\nThis is the bishop's storehouse. The storehouse"+
+    "\nis part of our welfare system and provides necessary items for people in"+
+    "\nneed. Volunteers and offerings are always welcome! See the game hint for"
+            + "\nmore details." ;
+    
+    // set a storehouse location and leave a hint
+    loc = new Location();
+    loc.setDescription(toolshed + "\nTo see a list of the provisions available,"
+            + "\ngo to the game menu and select option 2. Offerings above"
+            + "\n12% provide the largest crop yield opportunity.");
+    loc.setSymbol("+++");
+    theMap.setLocation(0,3,loc);
     
     
     
+    //UNDEVELOPED LAND----------------------------------------------------------
+    // define a string for the expansion location
+
+    String expansion ="\nThinking about relocating? We have reserved this land" +        
+                      "\nfor new residential properties and business development," +
+                      "\nin order to meet the needs of our growing community."; 
+                   
+    loc = new Location();
     
+    //use setters in the Location class to set the description and symbol
+    loc.setDescription(expansion + "\nLand prices range between 17 and 27 "
+            + "\nbushels per acre, and change yearly.");
+    loc.setSymbol(">>>");
+
+    //set this location object in each cell of the array in column 0
+    for(int i = 1; i < MAX_ROW; i++){
+      theMap.setLocation(i,2,loc);
     
+    }
     
     theGame.setMap(theMap);
             
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
 }
-
-
 
 
 }//end of class
